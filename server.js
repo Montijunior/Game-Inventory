9a32a5478bdfedd21e44d2da4ae22eef936e2e5f
Monti junior <montiabouou@gmail.com>
2025-01-07 16:16:02 +0100

    add developer and category routes

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 require("dotenv").config();
 const express = require("express");
+const CategoryRoutes = require("./routes/CategoryRoutes");
+const DeveloperRoutes = require("./routes/DeveloperRoutes");
 const app = express();
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.set("view engine", "ejs");
+
+app.use("/categories", CategoryRoutes);
+app.use("/developers", DeveloperRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
