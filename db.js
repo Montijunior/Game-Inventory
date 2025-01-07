20c6979c2680b1fb7c929c48abaaee19ecfbf85e
Monti junior <montiabouou@gmail.com>
2025-01-07 14:34:12 +0100

    connect single install of pg.pool

--- db.js
+++ db.js
@@ -5,4 +5,15 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
+async function connect() {
+  try {
+    const client = await pool.connect();
+    console.log("Connected to the database");
+    client.release();
+  } catch (err) {
+    console.error("Connection error", err);
+  }
+}
+connect();
+
 module.exports = pool;
